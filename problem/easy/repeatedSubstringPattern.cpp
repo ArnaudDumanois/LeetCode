8aedfc40c2ef0f36d91a14e18130dfe34a85069b
Arnaud <arnaud.dumanois@etu.univ-cotedazur.fr>
2024-06-17 23:02:49 +0200

    feat: easy problems

---
+++ problem/easy/repeatedSubstringPattern.cpp
@@ -0,0 +1,19 @@
+//
+// Created by arnau on 17/06/2024.
+//
+bool repeatedSubstringPattern(string s) {
+    int n = s.size();
+    for (int i = 1; i <= n / 2; i++) {
+        if (n % i == 0) {
+            string sub = s.substr(0, i);
+            string temp = "";
+            for (int j = 0; j < n / i; j++) {
+                temp += sub;
+            }
+            if (temp == s) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
